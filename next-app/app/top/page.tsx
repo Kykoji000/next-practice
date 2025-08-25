5d89adf2de8a01742bc26b15e63997cac8fb0838
YUSEMAN <authority1001x@gmail.com>
2025-08-25 11:07:33 +0900

    create onSubmit system

--- next-app/app/top/page.tsx
+++ next-app/app/top/page.tsx
@@ -2,14 +2,16 @@
 
 function Page() {
     return (
-        <input
-            className="p-2 rounded-xl border"
-            
-            onChange={(e) => {
-                alert(e.target.value);
+        <form
+            onSubmit={() => {
+                alert("送信しました");
             }}
-        />
-    )
+        >
+            <label htmlFor="name">名前: </label>
+            <input className="p-1 rounded-xl border" name="name" />
+            <button className="p-1 rounded-xl border">送信</button>
+        </form>
+    );
 }
 
 export default Page;
